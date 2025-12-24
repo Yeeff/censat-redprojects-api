8ddec5fbd79322e7d4fccac3ac3d1a32cff888e7
Yeff <yefrygf@gmail.com>
2025-12-24 14:59:02 -0500

    First commit

---
+++ src/main/java/com/censat/redd_projects_api/ReddProjectsApiApplication.java
@@ -0,0 +1,13 @@
+package com.censat.redd_projects_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddProjectsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReddProjectsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/censat/redd_projects_api/ReddProjectsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.censat.redd_projects_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddProjectsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
